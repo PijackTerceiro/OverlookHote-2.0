ea2b8e6e0da8ca1f56688e0d3f3c16a89ede2df2
santimgh <santimiguelgomez@gmail.com>
2022-05-31 15:16:14 +0200

    Cleaner code

--- src/OverlookPackage/RoomList.java
+++ src/OverlookPackage/RoomList.java
@@ -17,8 +17,41 @@ public class RoomList
   @XmlElement(name = "room", type = Room.class)
   private List<Room> rooms = new ArrayList<Room>();
 
+
   public RoomList()
   {
+    Room room1 = new Room("Room", 1, 1, 129, false);
+    Room room2 = new Room("Room", 2, 1, 129, false);
+    Room room3 = new Room("Room", 3, 1, 129, false);
+    Room room4 = new Room("Room", 4, 1, 129, false);
+    Room room5 = new Room("Room", 5, 1, 129, false);
+    Room room6 = new Room("Room", 6, 1, 129, false);
+    Room room7 = new Room("Room", 7, 1, 129, false);
+    Room room8 = new Room("Room", 8, 1, 129, false);
+    Room room9 = new Room("Room", 9, 1, 129, false);
+    Room room10 = new Room("Room", 10, 1, 129, false);
+    Room room11 = new Room("Room", 11, 1, 129, false);
+    Room room12 = new Room("Room", 12, 2, 169, false);
+    Room room13 = new Room("Suite", 38, 2, 339, false);
+    Room room14 = new Room("Suite", 39, 2, 339, false);
+    Room room15 = new Room("Suite", 40, 2, 339, false);
+    Room room16 = new Room("Suite", 41, 2, 339, false);
+    rooms.add(room1);
+    rooms.add(room2);
+    rooms.add(room3);
+    rooms.add(room4);
+    rooms.add(room5);
+    rooms.add(room6);
+    rooms.add(room7);
+    rooms.add(room8);
+    rooms.add(room9);
+    rooms.add(room10);
+    rooms.add(room11);
+    rooms.add(room12);
+    rooms.add(room13);
+    rooms.add(room14);
+    rooms.add(room15);
+    rooms.add(room16);
   }
 
   public RoomList(List<Room> rooms)
@@ -35,15 +68,17 @@ public class RoomList
   public void setRooms(List<Room> rooms)
   {
     this.rooms=rooms;
+    dataWriter();
   }
 
   public void addRoom(Room room)
   {
     rooms.add(room);
+    dataWriter();
   }
 
   //Method to retrieve all the rooms from xml
-  public List<Room> getAllRooms()
+  public List<Room> dataWriter()
   {
 
     {
@@ -74,6 +109,7 @@ public class RoomList
   public void setRoom(Room room, int index)
   {
     rooms.add(index, room);
+    dataWriter();
   }
 
   public Room getRoomByNumber(int roomNumber)
@@ -87,6 +123,7 @@ public class RoomList
         room = rooms.get(i);
       }
     }
+    dataWriter();
     return room;
   }
 
@@ -100,6 +137,7 @@ public class RoomList
         ind = i;
       }
     }
+    dataWriter();
     return ind;
   }
 
@@ -115,6 +153,8 @@ public class RoomList
       if(roomNumber == rooms.get(i).getRoomNum())
       {
         rooms.get(i).setInReservation(true);
+        dataWriter();
+        break;
       }
     }
   }
--- src/OverlookPackage/Test.java
+++ src/OverlookPackage/Test.java
@@ -10,49 +10,15 @@ public class Test
 {
   public static void main(String[] args)
   {
-    Room room1 = new Room("Room", 1, 1, 129, false);
-    Room room2 = new Room("Room", 2, 1, 129, false);
-    Room room3 = new Room("Room", 3, 1, 129, false);
-    Room room4 = new Room("Room", 4, 1, 129, false);
-    Room room5 = new Room("Room", 5, 1, 129, false);
-    Room room6 = new Room("Room", 6, 1, 129, false);
-    Room room7 = new Room("Room", 7, 1, 129, false);
-    Room room8 = new Room("Room", 8, 1, 129, false);
-    Room room9 = new Room("Room", 9, 1, 129, false);
-    Room room10 = new Room("Room", 10, 1, 129, false);
-    Room room11 = new Room("Room", 11, 1, 129, false);
-    Room room12 = new Room("Room", 12, 2, 169, false);
-    Room room13 = new Room("Suite", 38, 2, 339, false);
-    Room room14 = new Room("Suite", 39, 2, 339, false);
-    Room room15 = new Room("Suite", 40, 2, 339, false);
-    Room room16 = new Room("Suite", 41, 2, 339, false);
-
 
     RoomList roomList = new RoomList();
+    Room nRoom = new Room("Suite", 23, 2, 339, false);
+    roomList.dataWriter();
+    roomList.addRoom(nRoom);
 
-    roomList.addRoom(room1);
-    roomList.addRoom(room2);
-    roomList.addRoom(room3);
-    roomList.addRoom(room4);
-    roomList.addRoom(room5);
-    roomList.addRoom(room6);
-    roomList.addRoom(room7);
-    roomList.addRoom(room8);
-    roomList.addRoom(room9);
-    roomList.addRoom(room10);
-    roomList.addRoom(room11);
-    roomList.addRoom(room12);
-    roomList.addRoom(room13);
-    roomList.addRoom(room14);
-    roomList.addRoom(room15);
-    roomList.addRoom(room16);
-
-    roomList.getRoomByNumber(2).setPrice(210);
-    roomList.setRoomIsInReservation(roomList.getRoomByNumber(2).getRoomNum());
+    System.out.println(roomList);
 
 
-
-    System.out.println(roomList.getAllRooms());
 
   }
 }
